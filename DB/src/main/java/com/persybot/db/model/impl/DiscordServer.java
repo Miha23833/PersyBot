255b41c05d5a1ca1a3220409776893ee233f5624
Byoliee <mu.korepanov@gmail.com>
2021-09-26 02:13:45 +0300

    [PERSY-2] Added cache

--- DB/src/main/java/com/persybot/db/model/impl/DiscordServer.java
+++ DB/src/main/java/com/persybot/db/model/impl/DiscordServer.java
@@ -1,12 +1,19 @@
 package com.persybot.db.model.impl;
 
 import com.persybot.db.model.HbTable;
+import org.hibernate.annotations.Cache;
+import org.hibernate.annotations.CacheConcurrencyStrategy;
 import org.hibernate.annotations.Table;
 
-import javax.persistence.*;
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
 
 @Entity
 @Table(appliesTo = "DiscordServer")
+@Cache(usage = CacheConcurrencyStrategy.READ_WRITE)
 public class DiscordServer implements HbTable {
     @Id
     @GeneratedValue(strategy = GenerationType.TABLE)
