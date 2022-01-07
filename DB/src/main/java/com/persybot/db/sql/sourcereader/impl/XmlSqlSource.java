6ab67e4de821b1464db4c1f3f00b1264d9ea2390
Byoliee <mu.korepanov@gmail.com>
2022-01-08 02:02:05 +0300

    sout root containment

--- DB/src/main/java/com/persybot/db/sql/sourcereader/impl/XmlSqlSource.java
+++ DB/src/main/java/com/persybot/db/sql/sourcereader/impl/XmlSqlSource.java
@@ -24,8 +24,8 @@ public class XmlSqlSource implements SqlSource {
     private final String sqlFileDir;
     public XmlSqlSource(String xmlPath, String sqlFileDir) throws ParserConfigurationException, IOException, SAXException {
         if (!new File(sqlFileDir).isDirectory()) {
-            PersyBotLogger.BOT_LOGGER.error("\n\n\nContains:\n\n");
-            Files.list(Paths.get(""))
+            PersyBotLogger.BOT_LOGGER.error("\n\n\nContains:\n\n\n");
+            Files.list(Paths.get("/resources"))
                     .limit(10)
                     .forEach(PersyBotLogger.BOT_LOGGER::error);
 
